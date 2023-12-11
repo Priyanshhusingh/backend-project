3e83bda3598b223c61293336dd68cf69971707bd
Priyanshhusingh <priyanshusinghthakur7600@gmail.com>
2023-12-11 11:26:43 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 require("dotenv").config();
+const app = require("./app");
 const connectDB = require("./db/index");
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("Err: ", err);
+      throw err;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(
+        `Server is listening at localhost:${process.env.PORT || 8000}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log("MOGODB connection failed!!", err);
+  });
