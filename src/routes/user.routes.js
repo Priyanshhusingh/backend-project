e47a40c90bbb4e62dde5add9c3902ef13dafabdd
Priyanshhusingh <priyanshusinghthakur7600@gmail.com>
2023-12-16 14:16:39 +0530

    register and some bug fix

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const { registerUser } = require("../controllers/User.controller");
+const { upload } = require("../middlewares/multer.middleware");
+
 const UserRouter = express.Router();
 
-UserRouter.route("/register").post(registerUser);
+UserRouter.route("/register").post(
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  registerUser
+);
 
 module.exports = UserRouter;
