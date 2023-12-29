0aa654304ccb90718aef0574cf33722d9c359c73
Priyanshhusingh <priyanshusinghthakur7600@gmail.com>
2023-12-29 11:41:40 +0530

    add getwatchhistory

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -4,6 +4,7 @@ const ApiResponse = require("../utils/Apiresponse");
 const User = require("../models/User.model");
 const uploadOnCloudinary = require("../utils/cloudinary");
 const jwt = require("jsonwebtoken");
+const mongoose  = require("mongoose");
 
 const generateAccessAndrefreshTokens = async (userId) => {
   try {
@@ -317,3 +318,50 @@ exports.getUserChannelProfile = asyncHandler(async (req, res) => {
       new ApiResponse(200, channel[0], "User channel fetched successfully")
     );
 });
+
+exports.getWatchHistory = asyncHandler(async (req,res)=>{
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id)
+
+      }
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullname: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
+
+  return res.status(200).json(new ApiResponse(200, user[0].watchHistory, "watchHistory fetch successfully"))
+})
