f279955c8da493265a0a45c17acda2915ba9359a
Priyanshhusingh <priyanshusinghthakur7600@gmail.com>
2023-12-29 11:52:20 +0530

    all route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,9 +4,16 @@ const {
   loginUser,
   logOutuser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetail,
+  updateUserAvatar,
+  getUserChannelProfile,
+  getWatchHistory,
 } = require("../controllers/User.controller");
 const { upload } = require("../middlewares/multer.middleware");
 const { verifyJWT } = require("../middlewares/auth.middleware");
+const multer = require("multer");
 
 const UserRouter = express.Router();
 
@@ -22,4 +29,14 @@ UserRouter.route("/login").post(loginUser);
 //secures routes
 UserRouter.route("/logout").post(verifyJWT, logOutuser);
 UserRouter.route("/refresh-token").post(refreshAccessToken);
+UserRouter.route("/change-passwrd").post(verifyJWT, changeCurrentPassword);
+UserRouter.route("/current-user").get(verifyJWT, getCurrentUser);
+UserRouter.route("/update-account").patch(verifyJWT, updateAccountDetail);
+UserRouter.route("avatar").patch(
+  verifyJWT,
+  upload.single("avatar"),
+  updateUserAvatar
+);
+UserRouter.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+UserRouter.route("/watchHistory").get(verifyJWT, getWatchHistory);
 module.exports = UserRouter;
